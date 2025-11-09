f971b5e205c13d9f25ff33ec9ba6d914bb65ac51
kenny-204 <giwatwins@gmail.com>
2025-11-09 08:51:57 +0100

    trying to fix layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,8 +5,10 @@ export default function AuthLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen items-center justify-center bg-gray-50">
-        {children}
+      <body>
+        <main className="flex min-h-screen w-full items-center justify-center bg-gray-50">
+          {children}
+        </main>
       </body>
     </html>
   );
