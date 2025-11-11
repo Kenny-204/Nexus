56171d1a45432a2c9994acc085fd7440e6c10311
kenny-204 <giwatwins@gmail.com>
2025-11-11 12:31:45 +0100

    small fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
-  return <div>
-<Link href="login" >Login page</Link>
-  </div>;
+  return (
+    <div>
+      <Link href="login">Login page</Link>
+      <Link href="home">Home page</Link>
+    </div>
+  );
 }
