f937c93cd56dfb3857fdd765c1dc605acc3ddcc9
kenny-204 <giwatwins@gmail.com>
2025-11-12 13:11:59 +0100

    small fix

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDmiBWSQqR7B99tkoBBM6pc259Mdm-xiU",
-  authDomain: "nexus-96583.firebaseapp.com",
-  projectId: "nexus-96583",
-  storageBucket: "nexus-96583.firebasestorage.app",
-  messagingSenderId: "686023531106",
-  appId: "1:686023531106:web:5dec581a8380b546eacf3b",
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
