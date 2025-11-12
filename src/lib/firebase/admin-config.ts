ca06a224aa52a870897251f38eea931f6b776459
kenny-204 <giwatwins@gmail.com>
2025-11-12 13:00:52 +0100

    service account fix

--- src/lib/firebase/admin-config.ts
+++ src/lib/firebase/admin-config.ts
@@ -4,8 +4,9 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   const serviceAccount = require("@/serviceAccounts.json");
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount
-      //JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string),
+    credential: admin.credential.cert(
+      //serviceAccount
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string),
     ),
   });
 }
